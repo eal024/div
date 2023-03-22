a7df87644f58042a6cda38a8618a5d8fc43c44c9
Lamøy <eirik.lamoy@nav.no>
2023-03-22 10:11:41 +0100

    opdt. div. scr.

---
+++ 2023-03-22 shiny_app_figur_med_nedtrekksvalg.R
@@ -0,0 +1,55 @@
+
+
+library(shiny)
+library(highcharter)
+library(tidyverse)
+
+# Data
+Sys.setlocale("LC_CTYPE")
+data <- vroom::vroom("data/figurdata 4.1.csv") |>
+    mutate(versjon = ifelse(versjon == "H??y nasjonal vekst", "Hoy nasjonal vekst", versjon)
+    ) |> 
+    distinct()
+
+
+# Navngi valg i app
+omrade <- setNames(unique(data$omrade), unique(data$omrade) ) #
+versjon <- setNames( c("Hovedalternativet","Lav nasjonal vekst", "Hoy nasjonal vekst" ), c("Hovedalternativet","Lav nasjonal vekst", "Hoy nasjonal vekst" ) )
+
+
+ui <- fluidPage(
+    titlePanel("Figur med nedtrekksvalg"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput('omrade', 'Geografi', names(omrade) )
+        ),
+        mainPanel(
+            highchartOutput("plot")
+        )
+    )
+)
+
+server <- function(input, output) {
+    
+    dataset <- reactive({
+        subset_data <- data[data$omrade == input$omrade, ]
+        subset_data <- na.omit(subset_data)
+        subset_data
+    })  
+    output$plot <- renderHighchart({
+        
+        highchart() |>
+            hc_add_series( data = dataset(),
+                           type = "line",
+                           hcaes(y = value, x = ar, group = factor(versjon) )
+            ) %>%
+            hc_xAxis(title = list(text = "")) %>%
+            hc_yAxis(title = list(text = "Befolkningsantall")) |> 
+            hc_plotOptions( series = list( marker = list(enabled = F) )
+            )
+        
+    })  
+    
+}
+
+shinyApp(ui = ui, server = server)
